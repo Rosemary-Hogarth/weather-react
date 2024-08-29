4c9498cbfa64eebda7dd5b5e55a821d840910c25
Rosemary Hogarth <rohogarth@gmail.com>
2024-08-29 12:19:32 +0200

    Added axios, restructured components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Search from "./Search";
-import Date from "./Date";
 import Layout from "./Layout";
-import CityWeather from "./CityWeather";
-import CurrentWeather from "./CurrentWeather";
-import WeatherDetails from "./WeatherDetails";
 import Footer from "./Footer";
 import Forecast from "./Forecast";
 import "./style.css/Search.css"
-import "./style.css/Date.css";
 import "./style.css/styles.css";
-import "./style.css/CityWeather.css";
 import "./style.css/Forecast.css";
-import "./style.css/CurrentWeather.css";
 import "./style.css/Layout.css";
 import "./index.css";
 
@@ -21,12 +14,8 @@ export default function App() {
     <div className="App">
       <div class="container">
         <Layout />
-        <Search />
-        <Date time={8} day="Tuesday" date="27th August" />
-        <CityWeather city="Berlin" weather="Sunny" />
+        <Search defaultCity="Glasgow"/>
         <br />
-        <CurrentWeather degrees={30} />
-        <WeatherDetails humidity={20} pressure={30} wind={29} />
         <hr />
         <div className="forecast-container">
           <Forecast day="Wed" maxTemp={27} minTemp={15} />
--- src/Date.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Date(props) {
-  return (
-    <h1 id="date">
-      {props.time} O'Clock | {props.day} | {props.date}
-    </h1>
-  );
-}
